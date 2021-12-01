db073d025f06a826a0dbce2a58a7938762cf1d6e
Joseph Delong <joseph@delong.me>
2021-11-30 21:15:48 -0600

    add EIP712 signing for ERC1155

--- contracts/RedeemNFT.sol
+++ contracts/RedeemNFT.sol
@@ -21,18 +21,54 @@ interface IERC20 {
 }
 
 contract ERC1155Mint is ERC1155 {
+  bytes32 public immutable DOMAIN_SEPARATOR;
+  mapping(address => uint256) public nonces;
+
   constructor(address to, string memory _uri)
   ERC1155(
         _uri
   )
   {
+    uint256 chainId;
+    assembly {
+        chainId := chainid()
+    }
+    DOMAIN_SEPARATOR = keccak256(abi.encode(keccak256("EIP712Domain(uint256 chainId,address verifyingContract)"), chainId, address(this)));
     _mint(to, 0, 300, "");
+  }
+  // See https://eips.ethereum.org/EIPS/eip-191
+  string private constant EIP191_PREFIX_FOR_EIP712_STRUCTURED_DATA = "\x19\x01";
+  // keccak256("Permit(address owner,address spender,bool approved,uint256 nonce,uint256 deadline)");
+  bytes32 private constant PERMIT_SIGNATURE_HASH = keccak256("Permit(address owner,address spender,bool approved,uint256 nonce,uint256 deadline)");
+
+  function permit(
+      address owner_,
+      address spender,
+      bool approved,
+      uint256 deadline,
+      uint8 v,
+      bytes32 r,
+      bytes32 s
+  ) external {
+      require(owner_ != address(0), "ERC1155: Owner cannot be 0");
+      require(block.timestamp < deadline, "ERC1155: Expired");
+      bytes32 digest =
+          keccak256(
+              abi.encodePacked(
+                  EIP191_PREFIX_FOR_EIP712_STRUCTURED_DATA,
+                  DOMAIN_SEPARATOR,
+                  keccak256(abi.encode(PERMIT_SIGNATURE_HASH, owner_, spender, approved, nonces[owner_]++, deadline))
+              )
+          );
+      address recoveredAddress = ecrecover(digest, v, r, s);
+      require(recoveredAddress == owner_, "ERC1155: Invalid Signature");
+      _setApprovalForAll(owner_, spender, approved);
   }
 }
 
 contract RedeemNFT is ERC1155Receiver {
   IERC20 public immutable token;
-  ERC1155 public immutable nft;
+  ERC1155Mint public immutable nft;
 
   constructor(address _token, string memory _uri)
   {
@@ -60,7 +96,20 @@ contract RedeemNFT is ERC1155Receiver {
       redeemERC20ForNFT(owner);
   }
 
-  function redeemNFTForERC20() external {
+  function permitAndRedeemNFTForERC20(
+        address owner,
+        address spender,
+        bool approved,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+      nft.permit(owner, spender, approved, deadline, v, r, s);
+      redeemNFTForERC20();
+  }
+
+  function redeemNFTForERC20() internal {
     uint256 nftBalance = nft.balanceOf(msg.sender, 0);
     nft.safeTransferFrom(msg.sender, address(this), 0, nftBalance, "");
     token.transfer(msg.sender, nftBalance * 10**18);
